7568052864aded994b62fa7b769ead1701b3afba
Yixin Zhu <yixinzhu.xjtu@gmail.com>
2016-04-19 16:05:47 -0700

    update bibtex

--- HumanReconstruction/main.cpp
+++ HumanReconstruction/main.cpp
@@ -12,12 +12,11 @@ furnished to do so, subject to the following conditions:
 The above copyright notice and this permission notice shall be included in
 all copies or substantial portions of the Software.
 If the code is used in an article, the following publication shall be cited:
-@InProceedings{CVPR16_Chair,
-author = {Yixin Zhu, Chenfanfu Jiang, Yibiao Zhao, Demetri Terzopoulos and Song-Chun Zhu},
-title = {Inferring Forces and Learning Human Utilities From Videos},
-booktitle = {IEEE Conference on Computer Vision and Pattern Recognition (CVPR)},
-year = {2016}
-}
+@InProceedings{cvpr2016chair,
+    author = {Zhu, Yixin and Jiang, Chenfanfu and Zhao, Yibiao and Terzopoulos, Demetri and Zhu, Song-Chun},
+    title = {Inferring Forces and Learning Human Utilities From Videos},
+    booktitle = {IEEE Conference on Computer Vision and Pattern Recognition (CVPR)},
+    year = {2016}}
 */
 #include <stdio.h>
 #include <stdlib.h>
